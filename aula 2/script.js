e852aa61365c86539fea994855179656c3869ad7
Nyer <Ravannyer40@gmail.com>
2025-09-19 11:57:30 -0300

    consertei pequenos erros semânticos

--- aula 2/script.js
+++ aula 2/script.js
@@ -1,22 +1,26 @@
 function adicionarTarefa(){
     //recebe valor dio input do usuário
-    var inputTarefa = document.getElementById("inputTarefa")
-    var tarefa = inputTarefa.value
-        //se o valor do input for vazio, mostre uma mensagem de erro para o usuário
-        if (tarefa == "") {
-        alert("Adicione algo")
+    const inputTarefa = document.getElementById("inputTarefa")
+    var tarefa = inputTarefa.value.trim()
+    const mensagem = document.getElementById("mensagem")
+        
+    //se o valor do input for vazio, mostre uma mensagem de erro para o usuário
+    if (tarefa == "") {
+        var mensagemErro = "Digite uma tarefa para adicioná-la a sua lista"
+        mensagem.textContent = mensagemErro
+        mensagem.style.color = "#a34743"
         return
+    } else {
+        //mensagem de tarefa adicionada com sucesso
+        var mensagemSucesso = "Tarefa adicionada com sucesso!"
+        mensagem.textContent = mensagemSucesso
+        mensagem.style.color = "#28a745"
+        //cria novo item (li) e insere na (lista ul)
+        const ListasTarefas = document.getElementById("ListasTarefas")
+        var novaTarefa = document.createElement("li")
+        novaTarefa.textContent = tarefa
+        ListasTarefas.appendChild(novaTarefa)
     }
-
-    //cria novo item (li) e insere na (lista ul)
-    var ListasTarefas = document.getElementById("ListasTarefas")
-    var novaTarefa = document.createElement("li")
-    novaTarefa.textContent = tarefa
-    ListasTarefas.appendChild(novaTarefa)
-
-    //mensagem de tarefa adicionada com sucesso
-    var mensagem = "Tarefa adicionada com sucesso"
-    document.getElementById("mensagem").textContent = mensagem
 
     //limpa o input do usuário
     inputTarefa.value = ""
